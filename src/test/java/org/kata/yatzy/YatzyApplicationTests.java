befc15680912560ab29d3a807979484ed6914393
Ludivine Orhant <ludivineorhant@gmail.com>
2024-01-16 10:46:31 +0100

    kata base setup

---
+++ src/test/java/org/kata/yatzy/YatzyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kata.yatzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatzyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
