b298d220845d3d3c90163d8bf1ef4599768e41a4
orhantl <ludivineorhant@gmail.com>
2024-01-29 11:50:02 +0100

    refacto 1 to 6 methods and refact small / large straight

--- src/main/java/org/kata/yatzy/Yatzy.java
+++ src/main/java/org/kata/yatzy/Yatzy.java
@@ -3,45 +3,59 @@ package org.kata.yatzy;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.OptionalInt;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Yatzy {
 
     public static int chance(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> list = List.of(d1, d2, d3, d4, d5);
-        return list.stream().mapToInt(Integer::intValue).sum();
+        return Stream.of(d1, d2, d3, d4, d5)
+                .mapToInt(Integer::intValue).sum();
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> list = List.of(d1, d2, d3, d4, d5);
-        return list.stream().filter(d -> d == 1).mapToInt(Integer::intValue).sum();
+        return sumSearchedOccurrence(d1, d2, d3, d4, d5, 1);
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> list = List.of(d1, d2, d3, d4, d5);
-        return list.stream().filter(d -> d == 2).mapToInt(Integer::intValue).sum();
+        return sumSearchedOccurrence(d1, d2, d3, d4, d5, 2);
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> list = List.of(d1, d2, d3, d4, d5);
-        return list.stream().filter(d -> d == 3).mapToInt(Integer::intValue).sum();
+        return sumSearchedOccurrence(d1, d2, d3, d4, d5, 3);
     }
 
     public static int fours(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> list = List.of(d1, d2, d3, d4, d5);
-        return list.stream().filter(d -> d == 4).mapToInt(Integer::intValue).sum();
+        return sumSearchedOccurrence(d1, d2, d3, d4, d5, 4);
     }
 
     public static int fives(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> list = List.of(d1, d2, d3, d4, d5);
-        return list.stream().filter(d -> d == 5).mapToInt(Integer::intValue).sum();
+        return sumSearchedOccurrence(d1, d2, d3, d4, d5, 5);
     }
 
     public static int sixes(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> list = List.of(d1, d2, d3, d4, d5);
-        return list.stream().filter(d -> d == 6).mapToInt(Integer::intValue).sum();
+        return sumSearchedOccurrence(d1, d2, d3, d4, d5, 6);
+    }
+
+    private static int sumSearchedOccurrence(int d1, int d2, int d3, int d4, int d5, int searchedOccurrence) {
+        return Stream.of(d1, d2, d3, d4, d5)
+                .filter(d -> d == searchedOccurrence)
+                .mapToInt(Integer::intValue).sum();
+    }
+
+    public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
+        return isSmallStraight(d1, d2, d3, d4, d5) ? 15 : 0;
+    }
+
+    public static boolean isSmallStraight(int d1, int d2, int d3, int d4, int d5) {
+        return List.of(d1, d2, d3, d4, d5).containsAll(List.of(1, 2, 3, 4, 5));
+    }
+
+    public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
+        return isLargeStraight(d1, d2, d3, d4, d5) ? 20 : 0;
+    }
+
+    private static boolean isLargeStraight(int d1, int d2, int d3, int d4, int d5) {
+        return List.of(d1, d2, d3, d4, d5).containsAll(List.of(2, 3, 4, 5, 6));
     }
 
     public static int threeOfAKind(int d1, int d2, int d3, int d4, int d5) {
@@ -113,40 +127,6 @@ public class Yatzy {
             return score * 2;
         else
             return 0;
-    }
-
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[0] == 1 &&
-                tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1)
-            return 15;
-        return 0;
-    }
-
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1
-                && tallies[5] == 1)
-            return 20;
-        return 0;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
